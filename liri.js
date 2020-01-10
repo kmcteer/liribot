862c482ce0f4c5d81fef91c436d27a765a3da2d3
Kaleb M <kay@Kalebs-MacBook-Air.local>
2020-01-09 19:14:10 -0800

    have some errors and kinks that needs to be worked out

---
+++ liri.js
@@ -0,0 +1,52 @@
+// hook up liri bot with twitter key by requiring
+// the ./ below tells javascript to look for the keys.js file
+var keys = require('./keys.js');
+
+var Spotify = require('node-spotify-api');
+
+// next, look up twitter npm package to see how to install
+ 
+var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});
+
+var getArtistNames = function(artist) {
+    return artist.name;
+} 
+{/* have spotify call out a song name when searched
+then tell the query to input the song name inside the function */}
+var getMeSpotify = function(songName) {
+
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+     console.log('Error occurred: ' + err);
+     return
+  }
+ 
+    var songs = data.tracks.items;
+    for(var i=0; i<songs.length; i++) {
+        console.log(i);
+        console.log('artist(s): ' + songs [i].artist.map(
+            getArtistNAmes
+        ));
+    console.log('song name: ' + songs[i].name);
+    console.log('preview song: ' + songs[i].preview_url);
+    console.log('album: ' + songs[i].album.name);
+    console.log('----------------------');
+
+
+    }
+
+console.log(data.tracks.items[0]); 
+}});
+
+var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'spotify-this-song':
+            getMeSpotify(functionData);
+            break;
+        default:
+        console.log('LIRI does not know that');
+    }
+}
